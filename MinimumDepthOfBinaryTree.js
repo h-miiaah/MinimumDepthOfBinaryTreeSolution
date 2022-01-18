d47d415eb39e62a26abcea1c3465d99302f7838e
h-miiaah <haim.miiaah5@gmail.com>
2022-01-17 23:24:41 -0500

    final return

--- MinimumDepthOfBinaryTree.js
+++ MinimumDepthOfBinaryTree.js
@@ -14,4 +14,5 @@
     if (!root) return 0;
     if (!root.left) return minDepth(root.right) + 1;
     if (!root.right) return minDepth(root.left) + 1;
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
